df7d0a5237592d57dc6d797b26c1ecf68aa91414
juliusjohnson2002-droid <juliusjohnson2002@gmail.com>
2026-05-03 17:04:00 -0700

    hour 11

---
+++ JJohnson_Hour11/Assets/Script/PreFabGenerator.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PreFabGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+         Instantiate(prefab);
+         }
+        if (Input.GetKeyDown(KeyCode.Space))
+         {
+             Instantiate(prefab, transform.position, transform.rotation);
+             }
+       
+}
+}
