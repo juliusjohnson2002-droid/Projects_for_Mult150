e601b3203c09b557dd90ce868e0245ecb44bc10c
juliusjohnson2002-droid <juliusjohnson2002@gmail.com>
2026-05-31 20:37:45 -0700

    please look at FinalNew

---
+++ JJohnson_FinalNew/Assets/Scripts/Goual scrip.cs
@@ -0,0 +1,25 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GoalScript : MonoBehaviour
+{
+    public bool isSolved = false;
+    // Start is called before the first frame update
+  
+   private void OntriggerEnter( Collider other)
+    {
+        GameObject collidedWith = collider.gameObject;
+        if(collidedWith.tag == gameoObject.tag)
+        {
+            solved = true;
+            other.GetComponent<Rigidbody>().enabled = false;
+            Destroy(collidedWith);
+        }
+    }
+   
+    
+}
+*/
